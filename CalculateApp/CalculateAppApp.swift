938761c688880371b10aaa7e9cff47b0fabb9ea0
solisiaa <bylautyata@mail.ru>
2026-09-18 12:55:51 +0300

    Initial Commit

---
+++ CalculateApp/CalculateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculateAppApp.swift
+//  CalculateApp
+//
+//  Created by Ангелина Щербова on 16.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct CalculateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
